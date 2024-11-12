1b628a0042855f5b670c3f3df6203a4cc9741ce7
Jonas <jonasdurau@gmail.com>
2024-11-11 23:37:25 -0300

    Project created

---
+++ src/main/java/com/jonasdurau/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.jonasdurau.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonasdurau/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasdurau.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
