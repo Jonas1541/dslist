92b7ecdd3f656ab28d13c9ee058bf9f53e5dde5a
Jonas <jonasdurau@gmail.com>
2024-11-14 22:11:36 -0300

    SQL, projection, get games by list

--- src/main/java/com/jonasdurau/dslist/dto/GameMinDTO.java
+++ src/main/java/com/jonasdurau/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.jonasdurau.dslist.dto;
 
 import com.jonasdurau.dslist.entities.Game;
+import com.jonasdurau.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -27,6 +28,14 @@ public class GameMinDTO {
         this.year = entity.getYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        this.id = projection.getId();
+        this.title = projection.getTitle();
+        this.year = projection.getYear();
+        this.imgUrl = projection.getImgUrl();
+        this.shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
